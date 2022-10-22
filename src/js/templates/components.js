a54ccd7c7db1af8cc54aafa364c70964a390cc64
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2022-10-21 22:20:41 -0300

    Update modal - contactFormHandler

--- src/js/templates/components.js
+++ src/js/templates/components.js
@@ -1,31 +1,34 @@
 export default class Components {
   static ContactFormDOM(name) {
     return `
-      <div id="contact_modal">
+      <div id="contact_modal" class="modal-bg">
         <div class="modal">
           <header>
             <h1>Contactez-moi ${name}</h1>
             <img class="close" src="assets/icons/close.svg" />
           </header>
-          <form>
-            <div class="formdata">
-              <label>Prénom</label>
-              <input id="firstname" />
-            </div>
-            <div class="formdata">
-              <label>Nom</label>
-              <input id="lastname" />
-            </div>
-            <div class="formdata">
-              <label>Email</label>
-              <input id="email" />
-            </div>
-            <div class="formdata message">
-              <label>Votre message</label>
-              <input id="message" />
-            </div>
-            <button class="submit-button">Envoyer</button>
-          </form>
+          <div class="modal-body">
+            <form id="form" name="contact" action="#" method="post" novalidate>
+              <div class="formData">
+                <label>Prénom</label>
+                <input id="firstname" class="text-control" name="firstname" type="text" required />
+              </div>
+              <div class="formData">
+                <label>Nom</label>
+                <input id="lastname" class="text-control" name="lastname" type="text" required />
+              </div>
+              <div class="formData">
+                <label>Email</label>
+                <input id="email" class="text-control" name="email" type="text" required />
+              </div>
+              <div class="formData message">
+                <label>Votre message</label>
+                <textarea id="message" name="message" class="text-control" required></textarea>
+              </div>
+
+              <button class="submit-button">Envoyer</button>
+            </form>
+          </div>
         </div>
       </div>
     `;
@@ -36,20 +39,17 @@ export default class Components {
       <div class="filters">
         <h3>Trier par</h3>
         <div class="filters-dropdown">
-          <button class="filters-selected">
+          <div class="filters-selected">
             <span class="filters-value">Popularité</span>
+          </div>
+          <button id="icon" class="filters-button">
             <i class="fa fa-chevron-down"></i>
           </button>
-          <div class="filters-container">
-            <ul class="filters-option">
-              <li class="filters-item">Popularité</li>
-              <li class="filters-item">Date</li>
-              <li class="filters-item">Titre</li>
-            </ul>
-            <button class="filters-close">
-              <i class="fa fa-chevron-up"></i>
-            </button>
-         </div>
+          <ul class="filters-option">
+            <li class="filters-item">Popularité</li>
+            <li class="filters-item">Date</li>
+            <li class="filters-item">Titre</li>
+          </ul>
         </div>
       </div>
     `;
--- src/js/utils/contactForm.js
+++ src/js/utils/contactForm.js
@@ -1,11 +1,14 @@
+import CheckIsInvalid from "./checkIsInvalid.js";
+
 export default class ContactFormUtils {
   constructor() {
     this._modal = document.querySelector("#contact_modal");
+    // this._formData = [ ...document.querySelectorAll(".formData") ];
   }
 
   openModal() {
     document.body.style.overflow = "hidden";
-    this._modal.style.display = "block";
+    this._modal.style.display = "flex";
   }
 
   closeModal() {
@@ -13,13 +16,36 @@ export default class ContactFormUtils {
     this._modal.style.display = "none";
   }
 
-  contactFormHandler() {}
+  isInvalid() {
+    const formData = [ ...document.querySelectorAll(".formData") ];
+    const check = new CheckIsInvalid(formData);
 
-  launcher() {
+    return (check.firstname("#firstname") || check.lastname("#lastname") || check.email("#email") || check.message("#message")) ? true : false;
+  }
+
+  contactFormHandler(e) {
+    const form = document.querySelector("#form");
+    const formData = new FormData(form);
+    const result = Object.fromEntries(formData.entries());
+
+    e.preventDefault();
+
+    if (this.isInvalid()) return;
+
+    console.log(result);
+
+    this.closeModal();
+  }
+
+  launch() {
     const openButton = document.querySelector(".open");
     const closeButton = document.querySelector(".close");
+    const submitButton = document.querySelector(".submit-button");
 
     openButton.addEventListener("click", () => this.openModal());
     closeButton.addEventListener("click", () => this.closeModal());
+    submitButton.addEventListener("click", this.contactFormHandler.bind(this));
   }
 }
+
+// localhost:8080/photographer.html?id=243
